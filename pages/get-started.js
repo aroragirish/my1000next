2d69bd004a265adbd0db9e2e58a3ea5f9c388644
aroragirish <aroragirish08@gmail.com>
2023-01-07 16:21:26 +0530

    Versioning untracked files

--- pages/get-started.js
+++
@@ -1,9 +0,0 @@
-import FormBannerComponent from '../components/custom/sections/formbannercomponent';
-
-const GetStarted = () => {
-  
-return <FormBannerComponent />
-
-}
-
-export default GetStarted
--- store/types.js
+++
@@ -1,4 +0,0 @@
-export const GET_USERS = 'GET_USERS';
-export const SAVE_USER = 'SAVE_USER';
-export const LOGOUT = 'LOGOUT';
-export const USER_NEEDS_REGISTRATION= 'USER_NEEDS_REGISTRATION';
