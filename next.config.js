ea73b2f7ca633bcbbbd789e7e3ad3dd5c0be8bc3
aroragirish <aroragirish08@gmail.com>
2023-01-07 19:32:34 +0530

    update config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   reactStrictMode: false,
+  ignoreBuildErrors: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
